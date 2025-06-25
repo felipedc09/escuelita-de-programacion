7576a5f0d721cf9407eaa898803662c881bd5a62
Felipe Duitama <felipedc09@gmail.com>
2025-06-25 03:10:33 +0000

    Refactor functions.js to enhance structure and add comments for clarity

--- functions.js
+++ functions.js
@@ -1,6 +1,84 @@
 // Functions
-function main() {
+// function nombreDeLaFunction(inputs/parametros) {
+//   // Código de la función
+//   return "Resultado de la función";
+// }
 
+// Tipos de datos
+const numero = 10; // Number
+const texto = "Hola, mundo"; // String
+
+let esFeliz = ""; // String - false
+esFeliz = "a"; // String - true
+esFeliz = 0; // number - false
+esFeliz = 1; // number - true
+esFeliz = false; // Boolean - false
+esFeliz = true; // Boolean - true
+
+
+if (esFeliz) {
+  console.log("¡Estoy feliz!");
+}
+
+// int numeroEntero = 5; // Integer
+// float numeroDecimal = 3.14; // Float
+// double numeroDoble = 2.71828; // Double
+// boolean esVerdadero = true; // Boolean
+// char caracter = 'A'; // Character
+// String textoLargo = "Este es un texto largo que puede contener múltiples líneas y caracteres especiales como ñ, á, é, í, ó, ú.";
+// Array < string > lista =["1", "2", "3", "4", "5"]; // Array
+
+
+
+// Definir una función
+
+// implementar una función
+
+// Ejecutar/usar una función
+
+function main() {
+  /////////////////////////////////////////////////////////////////
+
+  // const retornoDeLAFuncion = ejecucionDeLaFunction(parametros)
+  // console.log(retornoDeLAFuncion)
+
+  function identificarNecesidad(nombresClientes) {
+    const necesidades = []
+    for (let i = 0; i < nombresClientes.length; i++) { //i = i + 1
+      console.log("Identificar necesidad de cliente: " + nombresClientes[i]);
+      necesidades[i] = "necesidad " + nombresClientes[i];
+    }
+    return necesidades
+  }
+
+  //verbo
+  function conocerElProblema(negocio){
+    console.log("estudair el negocio: " + negocio)
+    conocer.log("revisar estado actual")
+    conocer.log("definir como quiero que funcione")
+
+  }
+  //Divide y venceras
+
+  function plantearSolucion(){
+// lluvia de ideas -- ideas
+// limitaciones
+  }
+
+  function createProgram() {
+    const necesidades = identificarNecesidad(["Juan", "Maria"])
+    conocerElProblema("ferreteria")
+    console.log("Plantear solucion")
+    console.log("Desarrollar la solucion")
+    console.log("Programar")
+    console.log("Probar")
+    console.log("Depurar")
+    console.log("Probar")
+    console.log("Entregar")
+  }
+
+  // proceso diferentes para crear un programa
+  createProgram()
 }
 
 main()
